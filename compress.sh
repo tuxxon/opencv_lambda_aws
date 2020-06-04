fd13ba6a60c2240f38d3eff2b2032bfd0b5ccadf
Gordon Ahn <tuxxon@nate.com>
2020-06-04 17:26:24 +0900

    Updated compress.sh

--- compress.sh
+++ compress.sh
@@ -1,6 +1,9 @@
 ZIPFILE=allinone.zip
-unzip cv2-python37.zip 
-cp app.py python/lib/python3.7/site-packages/
-cd python/lib/python3.7/site-packages/
-zip -r9 ../../../../$ZIPFILE .
-cd -
+zip -d $ZIPFILE app.py
+zip -g $ZIPFILE app.py
+
+#unzip cv2-python37.zip 
+#cp app.py python/lib/python3.7/site-packages/
+#cd python/lib/python3.7/site-packages/
+#zip -r9 ../../../../$ZIPFILE .
+#cd -
